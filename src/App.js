e5d66003504871c1cc1d653ee0d2dc87959ee7c0
Ivan <108135420+IvanBanifatsyeu@users.noreply.github.com>
2024-06-23 23:39:01 +0300

    test of rendering react, toBeInTheDocument, toHaveStyle, screen.debug, fireEvent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState(null);
+	const [toggle, setToggle] = useState(false);
+
+	 const handleOnClick = () => {
+	  setToggle(prev => !prev)
+	 }
+	useEffect(() => {
+	 setTimeout(() => {
+	 setData({})
+	 },100)
+	},[])
+
+	return (
+		<div>
+			{data && <div  style={{color: 'red'}}>data</div>}
+			{toggle===true && <div data-testid='toggle-elem'>toggle</div>}
+			<h1>Hello world</h1>
+			<button data-testid ='toggle-btn' onClick={handleOnClick}>click me</button>
+			<input type="text" placeholder="input value ..." />
+		</div>
+	);
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("TEST APP", () => {
+	test("renders learn react link", () => {
+		render(<App />);
+		const helloWorld = screen.getByText(/hello world/i);
+		const btn = screen.getByRole("button");
+		const input = screen.getByPlaceholderText(/input value/i);
+		expect(helloWorld).toBeInTheDocument();
+		expect(btn).toBeInTheDocument();
+		expect(input).toMatchSnapshot();
+	});
+	test("renders learn react link2", async () => {
+		render(<App />);
+		screen.debug();
+		const data = await screen.findByText(/data/i);
+		expect(data).toBeInTheDocument();
+		expect(data).toHaveStyle({ color: "red" });
+		screen.debug();
+	});
+
+	test("CLICK EVENT", () => {
+		render(<App />);
+
+		const btn = screen.getByTestId("toggle-btn");
+		expect(screen.queryByTestId("toggle-elem")).toBeNull();
+    fireEvent.click(btn)
+    expect(screen.queryByTestId("toggle-elem")).toBeInTheDocument();
+    fireEvent.click(btn)
+		expect(screen.queryByTestId("toggle-elem")).toBeNull();
+	});
 });
